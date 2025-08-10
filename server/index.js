11b073113df82c889bd7ccd0f02ed7d3f5b36f07
Adithyaharivpn <adithyaharivpn@gmail.com>
2025-08-10 21:30:10 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+var dotenv = require('dotenv')
+dotenv.config()
+const port = process.env.port
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
