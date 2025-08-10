eaa9c1a0dd6c1bc930753ac17b4a48ff502afb11
Adithyaharivpn <adithyaharivpn@gmail.com>
2025-08-10 23:12:16 +0530

    first commit

---
+++ server/database.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose')
+var dotenv = require('dotenv')
+
+dotenv.config()
+
+mongoose.connect(process.env.mongodb_url).then(()=>{
+    console.log("Database Connected")
+}).catch((error)=>{
+    console.log(error)
+})
