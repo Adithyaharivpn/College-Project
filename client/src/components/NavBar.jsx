cab9a8987760af2b76e9b020c149bd9997fd19f5
Adithyaharivpn <adithyaharivpn@gmail.com>
2025-08-11 23:04:19 +0530

    Added navbar and hero

---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,28 @@
+import {
+  AppBar,
+  Box,
+  Button,
+  IconButton,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" color="primary">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            HomeCrew
+          </Typography>
+          <Button color="secondary" variant="contained" sx={{mr:2}}>Post a Job</Button>
+          <Button color="inherit">Signup</Button>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
+
+export default NavBar;
